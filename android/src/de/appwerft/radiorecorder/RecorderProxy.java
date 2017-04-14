f71d6f20c63f0cfcb83ae8dd015befb7d511ff48
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2017-04-14 16:59:20 +0200

    doInBackground added

--- android/src/de/appwerft/radiorecorder/RecorderProxy.java
+++ android/src/de/appwerft/radiorecorder/RecorderProxy.java
@@ -10,6 +10,7 @@ package de.appwerft.radiorecorder;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
@@ -20,6 +21,8 @@ import org.appcelerator.kroll.KrollProxy;
 import org.appcelerator.kroll.annotations.Kroll;
 import org.appcelerator.titanium.util.Log;
 import org.appcelerator.titanium.util.TiConfig;
+
+import android.os.AsyncTask;
 
 // This proxy can be created by calling Radiorecorder.createExample({message: "hello world"})
 @Kroll.proxy(creatableInModule = RadiorecorderModule.class)
@@ -45,24 +48,34 @@ public class RecorderProxy extends KrollProxy {
 
 	@Kroll.method
 	public void start() {
+		AsyncTask<Void, Void, Void> doRequest = new RecordHandler();
+		doRequest.execute();
 
 	}
 
-	private void record() {
-		try {
-			URLConnection conn = new URL(url).openConnection();
-			InputStream is = conn.getInputStream();
-			OutputStream outstream = new FileOutputStream(new File(file));
-			byte[] buffer = new byte[4096];
-			int len;
-			long t = System.currentTimeMillis();
-			while ((len = is.read(buffer)) > 0
-					&& System.currentTimeMillis() - t <= duration) {
-				outstream.write(buffer, 0, len);
+	private final class RecordHandler extends AsyncTask<Void, Void, Void> {
+		@Override
+		protected Void doInBackground(Void... empty) {
+			try {
+				URLConnection conn = new URL(url).openConnection();
+				InputStream is = conn.getInputStream();
+				OutputStream outstream = new FileOutputStream(new File(file));
+				byte[] buffer = new byte[4096];
+				int len;
+				long t = System.currentTimeMillis();
+				while ((len = is.read(buffer)) > 0
+						&& System.currentTimeMillis() - t <= duration) {
+					outstream.write(buffer, 0, len);
+				}
+				outstream.close();
+			} catch (Exception e) {
+				System.out.print(e);
 			}
-			outstream.close();
-		} catch (Exception e) {
-			System.out.print(e);
+			return null;
+		}
+
+		protected void onPostExecute() {
 		}
 	}
+
 }
